966adc0897f4cd52750fc5e8228f808c1499df6f
Julie Eustache <jeustache90@yahoo.com>
2017-12-11 23:11:33 -0500

    first commit

---
+++ public/main.js
@@ -0,0 +1,75 @@
+let thumbUp = document.getElementsByClassName("fa-thumbs-up");
+let trash = document.getElementsByClassName("fa-trash");
+let thumbDown = document.getElementsByClassName("fa-thumbs-down");
+
+
+Array.from(thumbUp).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        console.log("pizza");
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp':thumbUp
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(thumbDown).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        console.log("click");
+        fetch('thumbs', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp':thumbDown
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        // you always have to skip one. start with 1 because that is how js is rendered
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          // fetch is a fancy way of doing an ajax resquest
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
